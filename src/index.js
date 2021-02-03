f1b37b37671330db5e18eb1783816ed2b50070f9
AlwaysFocus <eliwhalen3@gmail.com>
2021-02-02 20:08:37 -0700

    Finished base tic-tac-toe game

--- src/index.js
+++ src/index.js
@@ -2,23 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+
+// Example of a function component in react
+// function Square (props) {
+//       return (
+//         <button className="square" onClick={props.onClick}>
+//             {props.value}
+//         </button>
+//       );
+//   }
+
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        // Create a copy of the squares array
+        // this uses the concept of immutability and 
+        // will allow us to view the history of the game
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
+      }
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (<Square 
+                    value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+
+                />
+            );
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares)
+      let status;
+      if (winner) {
+          status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -42,6 +85,8 @@ class Square extends React.Component {
       );
     }
   }
+  
+
   
   class Game extends React.Component {
     render() {
@@ -67,3 +112,22 @@ class Square extends React.Component {
   );
   
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
